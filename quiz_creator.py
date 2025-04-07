a856f2796d55d009e96eef570b1982aac29cda75
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-04-07 10:40:25 +0800

    saving the question and answer success

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,17 @@
+quiz = {}
+
 # ask user for questions with choices a b c d
+quiz["question"] = input("Enter a question: ")
+choice_1 = input("Choice 1 (a): ")
+choice_2 = input("Choice 1 (b): ")
+choice_3 = input("Choice 1 (c): ")
+choice_4 = input("Choice 1 (d): ")
+
 # ask for the right answer or choice
+quiz["correct_choice"] = input("Correct answer: ").lower()
+
 # save the data
+
 # exit until the user exits
+
+print(quiz)
