4a93549969db64a90e416e8ea908dc12f2baeebc
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-04-30 08:51:45 +0800

    quiz program initial pseudocode

---
+++ quiz.py
@@ -0,0 +1,9 @@
+# display quiz details
+# initialize score to 0
+# read the quiz file 
+# display a random question
+# ask user for their answer
+# display the correct answer
+# if answer is correct add 1 else no score
+# repeat until no question left
+# display final score
